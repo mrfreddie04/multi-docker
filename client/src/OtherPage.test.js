d4cc90a9be914568eed199a79964283455cf19dc
mrfreddie04 <mrfreddie04@yahoo.com>
2022-10-26 01:14:45 -0400

    Fix failing test

--- client/src/OtherPage.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import OtherPage from './OtherPage';
-
-test('renders learn react link', () => {
-  render(<OtherPage />);
-  const linkElement = screen.getByText(/I am the other page/i);
-  expect(linkElement).toBeInTheDocument();
-});
