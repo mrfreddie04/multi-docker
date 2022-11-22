f41d0b762e0879204bae00aa49cbdd82df56386f
root <mrfreddie04@yahoo.com>
2022-11-21 19:22:39 -0500

    Clean up tests

--- client/src/OtherPage.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import OtherPage from './OtherPage';
-
-test('renders OtherPage without crashing', () => {
-  render(<OtherPage />);
-  const linkElement = screen.getByText(/Go Back Home/i);
-  expect(linkElement).toBeInTheDocument();
-});
